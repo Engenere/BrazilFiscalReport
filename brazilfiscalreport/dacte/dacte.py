ccef95f6d28392b9f6dd365f77bd58cc0aab49a0
Lucas Svizzero Ribeiro <lucas.svi94@gmail.com>
2026-04-13 11:42:52 -0300

    fix: handle None values in inf_carga, inf_doc, and v_prest lists

--- brazilfiscalreport/dacte/dacte.py
+++ brazilfiscalreport/dacte/dacte.py
@@ -93,19 +93,19 @@ class Dacte(xFPDF):
 
         self.page_lines = 0
         self.inf_carga_list = []
-        for infQ in self.inf_carga:
+        for infQ in self.inf_carga or []:
             self.c_unid = extract_text(infQ, "cUnid")
             self.tp_media = extract_text(infQ, "tpMed")
             self.q_carga = extract_text(infQ, "qCarga")
             self.inf_carga_list.append((self.c_unid, self.tp_media, self.q_carga))
 
         self.inf_doc_list = []
-        for chave in self.inf_doc:
+        for chave in self.inf_doc or []:
             self.chave = extract_text(chave, "chave")
             self.inf_doc_list.append(self.chave)
 
         self.comp_list = []
-        for comp in self.v_prest.findall(f"{URL}Comp"):
+        for comp in self.v_prest.findall(f"{URL}Comp") or []:
             self.xNome = extract_text(comp, "xNome")
             self.vComp = extract_text(comp, "vComp")
             self.comp_list.append((self.xNome, self.vComp))
